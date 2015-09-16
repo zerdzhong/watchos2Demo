6d1c3fb21f1e96978a6fd38d7fa8850b3a070e8e
zhongzhendong <zhongzhendong@youku.com>
2015-09-16 16:54:37 +0800

    add complication

--- watchos2 WatchKit Extension/ComplicationController.m
+++ watchos2 WatchKit Extension/ComplicationController.m
@@ -36,7 +36,30 @@
 
 - (void)getCurrentTimelineEntryForComplication:(CLKComplication *)complication withHandler:(void(^)(CLKComplicationTimelineEntry * __nullable))handler {
     // Call the handler with the current timeline entry
-    handler(nil);
+    
+    // Get the current complication data from the extension delegate.
+    
+    CLKComplicationTimelineEntry* entry = nil;
+    NSDate* now = [NSDate date];
+    
+    // Create the template and timeline entry.
+    if (complication.family == CLKComplicationFamilyModularLarge) {
+        CLKComplicationTemplateModularLargeStandardBody* t =
+        [[CLKComplicationTemplateModularLargeStandardBody alloc] init];
+        t.headerTextProvider = [CLKSimpleTextProvider textProviderWithText:@"Hello World"];
+        t.body1TextProvider = [CLKSimpleTextProvider textProviderWithText:@"now"];
+        t.body2TextProvider = [CLKTimeIntervalTextProvider textProviderWithStartDate:[NSDate date] endDate:[NSDate dateWithTimeIntervalSinceNow:((6*60*60)+(18*60))]];
+        
+        // Create the entry.
+        entry = [CLKComplicationTimelineEntry entryWithDate:now
+                                       complicationTemplate:t];
+    }
+    else {
+        // ...configure entries for other complication families.
+    }
+    
+    // Pass the timeline entry back to ClockKit.
+    handler(entry);
 }
 
 - (void)getTimelineEntriesForComplication:(CLKComplication *)complication beforeDate:(NSDate *)date limit:(NSUInteger)limit withHandler:(void(^)(NSArray<CLKComplicationTimelineEntry *> * __nullable entries))handler {
@@ -46,7 +69,39 @@
 
 - (void)getTimelineEntriesForComplication:(CLKComplication *)complication afterDate:(NSDate *)date limit:(NSUInteger)limit withHandler:(void(^)(NSArray<CLKComplicationTimelineEntry *> * __nullable entries))handler {
     // Call the handler with the timeline entries after to the given date
-    handler(nil);
+    
+    CLKComplicationTimelineEntry* entry1 = nil;
+    CLKComplicationTimelineEntry* entry2 = nil;
+    NSDate* oneMin = [NSDate dateWithTimeInterval:60*10 sinceDate:date];
+    NSDate* twoMin = [NSDate dateWithTimeIntervalSinceNow:60*2];
+    
+    // Create the template and timeline entry.
+    if (complication.family == CLKComplicationFamilyModularLarge) {
+        CLKComplicationTemplateModularLargeStandardBody* t =
+        [[CLKComplicationTemplateModularLargeStandardBody alloc] init];
+        t.headerTextProvider = [CLKSimpleTextProvider textProviderWithText:@"Hello World"];
+        t.body1TextProvider = [CLKSimpleTextProvider textProviderWithText:@"十分钟过去了"];
+        
+        // Create the entry.
+        entry1 = [CLKComplicationTimelineEntry entryWithDate:oneMin
+                                       complicationTemplate:t];
+        
+        CLKComplicationTemplateModularLargeStandardBody* t1 =
+        [[CLKComplicationTemplateModularLargeStandardBody alloc] init];
+        t1.headerTextProvider = [CLKSimpleTextProvider textProviderWithText:@"Hello World"];
+        t1.body1TextProvider = [CLKSimpleTextProvider textProviderWithText:@"两分钟过去了"];
+        
+        // Create the entry.
+        entry2 = [CLKComplicationTimelineEntry entryWithDate:twoMin
+                                       complicationTemplate:t1];
+    }
+    else {
+        // ...configure entries for other complication families.
+    }
+
+    
+    
+    handler(@[entry1,entry2]);
 }
 
 #pragma mark Update Scheduling
